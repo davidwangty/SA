48b0ccfed9400872cf1d33cabb5312c9a6cb5816
Nini <apple@appletekiMacBook-Air-103.local>
2015-06-17 21:15:22 +0800

    hi

---
+++ create.php
@@ -0,0 +1,100 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Event Management</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+
+    <?php
+        include 'header.php';
+    ?>
+
+    <?php 
+        if(@$_SESSION['username1'] != null){
+            echo '<div class="container">
+                <div class="col-md-6 column">
+                    <form role="form" method="POST" action="commitact.php" Enctype="multipart/form-data">
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <h3>
+                        辦活動~~~!
+                        </h3>
+                        <div class="form-group">
+                            <label for="exampleInputlength">活動名稱</label><input type="text" class="form-control" name="name2">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputpublish">活動日期</label><input type="date" class="form-control" name="date">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputlength">活動資訊</label><textarea nrow="5" class="form-control" name="info"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputlength">活動圖片</label><input type="file" name="image">
+                        </div>
+                        <button type="submit" class="btn btn-default">送出</button>
+                        <br>
+                        <br>
+                        <br>
+                    </form>
+                </div>
+            </div>';
+        }else{
+            echo '<div class="container">
+                    <div class="col-md-6 column">
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <h3>
+                        請先登入
+                        </h3>
+                        <br>
+                        <br>
+                    </div>
+                  </div>';
+            $_SESSION['back'] = "create.php";
+            echo '<meta http-equiv=REFRESH CONTENT=1;url=login.php>';   
+        }
+    ?>
+
+    <!-- Footer -->
+    <?php
+        include 'footer.php';
+    ?>
+
+</body>
+
+</html>
--- show.php
+++ show.php
@@ -47,9 +47,68 @@
 <body id="page-top" class="index">
 
     <!-- Navigation -->
-    <?php
-        include 'header.php';
-    ?>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                      <a class="navbar-brand" href="index.php">
+                        <!-- <img alt="Brand" src="..."> -->
+                        Group 6
+                      </a>
+                    </div>
+                </div>
+            </div>
+
+            <form class="navbar-form navbar-left" role="search" method="POST" action="search.php">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="找活動囉~" name = "search">
+                </div>
+                <button type="submit" class="btn btn-default">搜尋</button>
+            </form>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <?php 
+                        if(@$_SESSION['username1'] != null){
+                            echo '<li>
+                                     <a href="create.php">辦活動</a>
+                                  </li>';
+                            echo '<li><a href = "manage.php">'.@$_SESSION['username1'].'</a><li><a href = "logout.php">登出</a>';
+                        }elseif(@$_SESSION['username2'] != null){
+                            echo '<li><a href = "manage.php">'.@$_SESSION['username2'].'</a><li><a href = "logout.php">登出</a>';
+                        }
+                        else{
+                            echo '<li><a href="register.php">註冊</a></li><li><a href="login.php">登入</a></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+        <div class="container-fluid" style="background:#eeeeee">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#info" style="color:darkblue">活動資訊</a></li>
+                    <li><a href="#QA" style="color:darkblue">Q&A </a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php
+                        $id = @$_GET['id'];
+                        echo '<li><a href = "add.php?id='.$id.'" style="color:darkblue">追蹤</a></li>';
+                        echo '<li><a href = "add.php?id='.$id.'" style="color:darkblue">參加</a></li>';
+                    ?>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+        <!-- /.container-fluid -->
+    </nav>
 
     <div class="container-fluid">
             <?php
